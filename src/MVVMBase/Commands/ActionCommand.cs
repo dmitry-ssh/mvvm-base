537765110116704eb23ff2735b194544ea49e015
dmitry-ssh <writedmitriy@gmail.com>
2023-12-25 11:58:08 +0100

    Added action command

---
+++ src/MVVMBase/Commands/ActionCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+
+namespace MVVMBase.Commands;
+
+public class ActionCommand<T> : ICommand
+{
+    private readonly Predicate<T?>? canExecute;
+    private readonly Action<T?>? execute;
+
+    public ActionCommand(Action<T?> execute, Predicate<T?>? canExecute = null)
+    {
+        this.canExecute = canExecute;
+        this.execute = execute;
+    }
+
+    public bool CanExecute(object? parameter) => canExecute?.Invoke((T?)parameter) ?? true;
+
+    public void Execute(object? parameter) => execute?.Invoke((T?)parameter);
+
+    public void OnCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public event EventHandler? CanExecuteChanged;
+}
